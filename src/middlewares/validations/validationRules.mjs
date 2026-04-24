825e4b4618cfd936f0dc0e67dde3938e569760ec
Alexander Maidana <alexanderemanuelmaidana@gmail.com>
2026-04-24 18:38:01 -0300

    Corregir validaciones en validationsRules.mjs y agregar validacion para los campos opcionales

--- src/middlewares/validations/validationRules.mjs
+++ src/middlewares/validations/validationRules.mjs
@@ -1,36 +1,74 @@
 import { body } from "express-validator";
 
 export const superheroValidations = [
-	// nombreSuperheroe
+  // nombreSuperheroe
   body("nombreSuperheroe")
-    .trim()
-    .notEmpty().withMessage("El nombre del superhéroe es requerido.")
-    .isLength({ min: 3 }).withMessage("El nombre del superhéroe debe tener al menos 3 caracteres.")
+    .trim() // Eliminar espacios al inicio y al final
+    .notEmpty().withMessage("El nombre del superhéroe es requerido.") // Validar que el campo no esté vacío
+    .isLength({ min: 3 }).withMessage("El nombre del superhéroe debe tener al menos 3 caracteres.") // Minimo de 3 caracteres
     .isLength({ max: 60 }).withMessage("El nombre del superhéroe no puede superar los 60 caracteres.")
-    .not().matches(/^\d+$/).withMessage("El nombre del superhéroe no puede contener solo números."),
-
-  // nombreReal
+    .escape(), // Máximo de 60 caracteres
   body("nombreReal")
     .trim()
     .notEmpty().withMessage("El nombre real es requerido.")
     .isLength({ min: 3 }).withMessage("El nombre real debe tener al menos 3 caracteres.")
     .isLength({ max: 60 }).withMessage("El nombre real no puede superar los 60 caracteres.")
-    .matches(/^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ\s]+$/).withMessage("El nombre real no puede contener números ni caracteres especiales."),
-
-  // edad
+    .escape(),
   body("edad")
     .trim()
     .notEmpty().withMessage("La edad es requerida.")
-    .isNumeric().withMessage("La edad debe ser un número.")
-    .isNumeric({ min: 0 }).withMessage("La edad no puede ser negativa."),
-
-  // poderes
+    .isNumeric().withMessage("La edad debe ser un número.") // Valida que se un valor númerico
+    .custom((value) => { // Valida que la edad sea mayor o igual a cero
+      if (Number(value) < 0) {
+        throw new Error("La edad no puede ser negativa");
+      }
+      return true;
+    })
+    .escape(),
   body("poderes")
-    .isArray({ min: 1 }).withMessage("Los poderes son requeridos."),
+    .isArray({ min: 1 }).withMessage("Los poderes son requeridos.")// Valida que poderes tenga almenos un elemento
+    .isString().withMessage("Todos los poderes deben ser un texto"), // Que sea array de strings
   body("poderes.*")
     .trim()
-    .notEmpty().withMessage("Los poderes no pueden quedar vacios.")
     .isLength({ min: 3 }).withMessage("Cada poder debe tener al menos 3 caracteres.")
     .isLength({ max: 60 }).withMessage("Cada poder no puede superar los 60 caracteres.")
-    //.not().matches(/^\d+$/).withMessage("Ninún poder puede contener sólo números."),
-];
+    .escape(),
+
+  // Validaciones para los campos opcionales
+  body("planetaOrgen")
+    .optional()
+    .trim()
+    .isLength({ min: 3 }).withMessage("El planeta de origen debe tener al menos 3 caracteres.")
+    .isLength({ max: 60 }).withMessage("El planeta de origen no puede superar los 60 caracteres.")
+    .escape(),
+  body("debilidad")
+    .optional()
+    .trim()
+    .isLength({ min: 3 }).withMessage("La debilidad debe tener almenos 3 carácteres.")
+    .isLength({ max: 60 }).withMessage("La debilidad no puede superar los 60 caracteres.")
+    .escape(),
+  body("creador")
+    .optional()
+    .trim()
+    .isLength({ min: 3 }).withMessage("El creador debe tener almenos 3 carácteres.")
+    .isLength({ max: 60 }).withMessage("El creador no puede superar los 60 caracteres.")
+    .escape(),
+   body("aliados")
+    .optional()
+    .isString().withMessage("Cada aliados debe ser un texto"),
+  body("aliados.*")
+    .optional()
+    .trim()
+    .isLength({ min: 3 }).withMessage("Cada aliado debe tener al menos 3 caracteres.")
+    .isLength({ max: 60 }).withMessage("Cada aliado no puede superar los 60 caracteres.")
+    .escape(),
+  body("enemigos")
+    .optional()
+    .isString().withMessage("Cada enemigo debe ser un texto"),
+  body("enemigos.*")
+    .optional()
+    .trim()
+    .isLength({ min: 3 }).withMessage("Cada enemigo debe tener al menos 3 caracteres.")
+    .isLength({ max: 60 }).withMessage("Cada enemigo no puede superar los 60 caracteres.")
+    .escape(),
+  ];
